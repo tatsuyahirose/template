d8b84b5577da2febfad709ccb5e37bfebb989176
tatsuya_hirose <tatuyahirose0805@gmail.com>
2018-02-08 01:22:02 +0900

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   // エントリーポイントの指定
   entry: './src/main.js',
@@ -7,5 +9,22 @@ module.exports = {
     path: `${__dirname}/build`,
     // 出力ファイル名
     filename: 'bundle.js'
+  },
+
+  // ソースマップを有効にする
+  devtool: 'source-map',
+  plugins: [
+    // JSファイルのminifyを実行する
+    new webpack.optimize.UglifyJsPlugin({
+      // minify時でもソースマップを利用する
+      sourceMap: true
+    })
+  ],
+
+  // ローカル開発用環境を立ち上げる
+  // ブラウザで http://localhost:8081/ でアクセスできるようになる
+  devServer: {
+    contentBase: 'build',
+    port: 8081
   }
 };
